67d59e1f653bce13c26211103805ce26f78d3149
SVitaly <user7_490@mail.ru>
2023-02-11 20:12:38 +0300

    решили задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+
+Console.WriteLine("Задайте натуральное число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте натуральное число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sumNaturalNumbers (int m, int n)
+{
+  if (m==n)
+  {
+    return m;
+  }
+  else
+  {
+    return m+sumNaturalNumbers((m+1), n);
+  }
+}
+
+Console.WriteLine($"Сумма натуральных чисел в заданном промежутке: {sumNaturalNumbers(m, n)}");
